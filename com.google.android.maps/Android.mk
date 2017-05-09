61a885a5641bc6603c4a1687cef2ba31bfa7985f
Nicola Corna <nicola@corna.info>
2017-05-09 09:19:42 +0200

    Fix mapsv1

--- com.google.android.maps/Android.mk
+++ com.google.android.maps/Android.mk
@@ -1,15 +1,18 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := com.google.android.maps
+LOCAL_MODULE := com.google.android.maps.jar
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := com.google.android.maps.jar
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_SRC_FILES := com.google.android.maps.jar
+LOCAL_PACKAGE_NAME := com.google.android.maps.jar
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := com.google.android.maps.xml
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := com.google.android.maps.xml
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := com.google.android.maps.xml
