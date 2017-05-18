833e83d89c9275b0b9ed75d889267611fd7784ab
Nicola Corna <nicola@corna.info>
2017-05-18 20:59:10 +0200

    Add MozillaNlpBackend and NominatimNlpBackend

---
+++ MozillaNlpBackend/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := MozillaNlpBackend
+LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea_20018.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
