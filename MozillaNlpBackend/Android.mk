c56cc2b9cb6aaa0a7f876b5e5786632ced53ae03
Simon Polack <simonpolack@googlemail.com>
2018-09-27 11:17:05 +0200

    MozillaNlp: Update to 1.4.0
    Version: 1.4.0 (20033)
    Source: https://f-droid.org/repo/org.microg.nlp.backend.ichnaea_20033.apk
    Date of pull: 2018-09-27

--- MozillaNlpBackend/Android.mk
+++ MozillaNlpBackend/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := MozillaNlpBackend
-LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea_20026.apk
+LOCAL_SRC_FILES := org.microg.nlp.backend.ichnaea_20033.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
