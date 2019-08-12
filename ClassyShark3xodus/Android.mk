51e2b469645e3f52de2a8c7baeaa6933adab87b8
danielk43 <kun@buffalo.edu>
2019-08-12 16:15:53 -0600

    Update ClassyShark3xodus to v1.0.8

--- ClassyShark3xodus/Android.mk
+++ ClassyShark3xodus/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := ClassyShark3xodus
-LOCAL_SRC_FILES := com.oF2pks.classyshark3xodus_7.apk
+LOCAL_SRC_FILES := com.oF2pks.classyshark3xodus_8.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
