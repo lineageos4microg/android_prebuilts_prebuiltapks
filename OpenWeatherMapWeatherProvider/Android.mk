e20d57e29d3aa4d2a2f7c00b8b25bbd605453b24
John Jacob <johnjacob@usa.com>
2019-11-21 16:49:38 -0500

    Initial commit. 1. Removed FakeStore, to be replaced with Phonesky. 2. Added Phonesky, with Phonesky.apk version 17.6.19 from open_gapps-arm64-9.0-pico-20191121. 3. Updated OpenWeatherMapWeatherProvider with OpenWeatherProvider-16.0-signed.apk from LineageOS extras.

--- OpenWeatherMapWeatherProvider/Android.mk
+++ OpenWeatherMapWeatherProvider/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := OpenWeatherMapWeatherProvider
-LOCAL_SRC_FILES := OpenWeatherMapWeatherProvider.apk
+LOCAL_SRC_FILES := OpenWeatherProvider-16.0-signed.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
---
+++ Phonesky/README.md
@@ -0,0 +1,3 @@
+# gapps_store
+Repository for my own patched Play store, signed with my own private key for use with my own MicroG enabled LineageOS ROM. This replaces the FakeStore from lineageos4microg/android_prebuilts_prebuiltapks.
+
