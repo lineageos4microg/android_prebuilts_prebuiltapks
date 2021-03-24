61d7e6298a62c3464e3a0bc06911f28d3eebdcb2
Simon Polack <s.polack@syseleven.de>
2021-03-24 10:56:13 +0100

    FDroid Privileged Extension: Update to 0.2.11
    Version 0.2.12 (2120)
    Source: https://f-droid.org/repo/org.fdroid.fdroid.privileged_2120.apk
    Date of pull: 2021-03-24

--- FDroidPrivilegedExtension/Android.mk
+++ FDroidPrivilegedExtension/Android.mk
@@ -11,7 +11,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := FDroidPrivilegedExtension
-LOCAL_SRC_FILES := org.fdroid.fdroid.privileged_2110.apk
+LOCAL_SRC_FILES := org.fdroid.fdroid.privileged_2120.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
